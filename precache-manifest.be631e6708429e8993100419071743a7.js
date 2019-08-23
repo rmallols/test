637444875d8447f68c77c8d61b267552f9e701da
Ricardo Mallols <ricardo.mallols@gmail.com>
2019-08-23 12:48:30 +0100

    Updates

---
+++ precache-manifest.be631e6708429e8993100419071743a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536ec113f0662e363ff333fe2547036d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3f122ad4321b67df7190",
+    "url": "/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2f96fdd6996641bbb518",
+    "url": "/test/static/js/2.92456c0e.chunk.js"
+  },
+  {
+    "revision": "3f122ad4321b67df7190",
+    "url": "/test/static/js/main.27119139.chunk.js"
+  },
+  {
+    "revision": "d51128ee3113cf026fe3",
+    "url": "/test/static/js/runtime~main.80713d17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
